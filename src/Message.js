b8fe63345cf634ff640d8b373c5932f1fa4a33b2
Gabriel Cioci <ciocigabi@gmail.com>
2020-10-05 16:49:21 +0300

    fix user data in message

--- src/Message.js
+++ src/Message.js
@@ -5,13 +5,13 @@ import {withRouter} from "react-router-dom"
 const auth = firebase.auth()
 
 const Message = (props) => {
-    const {text, uid, photoURL, displayName} = props.message;
-    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+    const {text, session_superchat, user_avatar, user_display_name} = props.message;
+    const messageClass = session_superchat === auth.currentUser.uid ? 'sent' : 'received';
     return (
         <div className={`message ${messageClass}`}>
-            <img src={photoURL} className="avatar" alt="avatar"/>
+            <img src={user_avatar} className="avatar" alt="avatar"/>
             <div className="message-body">
-                <span className="display-name">{displayName}</span>
+                <span className="display-name">{user_display_name}</span>
                 <span className="text-message">{text}</span>
             </div>
         </div>
